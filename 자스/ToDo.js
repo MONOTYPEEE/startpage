8b60ac9315cfa5b4774ca084b548f3a57fb9b0d3
monotypeee <spair0039@gmail.com>
2022-07-13 20:25:26 +0900

    할일 추가 가능
    근데 새로고침하면 증발

--- 자스/ToDo.js
+++ 자스/ToDo.js
@@ -2,15 +2,20 @@ const form = document.getElementById("ToDo-Form");
 const list = document.getElementById("ToDo-List");
 const input = form.querySelector("input");
 
-
-function whenSubmitTodo(){
+function printToDo(inputValue){
+    const listElement = document.createElement("li");
+    const listSpanElement = document.createElement("span");
+    listElement.appendChild(listSpanElement);
+    listSpanElement.innerText=inputValue;
+    list.appendChild(listElement);
+}
+function whenSubmitTodo(event){
     event.preventDefault();
     const inputValue = input.value;
     input.value = "";
+    printToDo(inputValue);
 }
 
-function printToDo(){
-    
-}
+
 
 form.addEventListener("submit",whenSubmitTodo)
