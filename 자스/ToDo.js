7a2a3b2fdb05c8bddec0d8c2740f3dc7a998096c
MONOTYPEEE <spair0039@gmail.com>
2022-08-01 15:24:53 +0900

    진짜 투두 삭제가 간으합니다

--- 자스/ToDo.js
+++ 자스/ToDo.js
@@ -2,15 +2,16 @@ const form = document.getElementById("ToDo-Form");
 const TdList = document.getElementById("ToDo-List");
 const TdInput = form.querySelector("#ToDo-Form input");
 
-function RemoveTodo(){
-    console.log("wow");
+function RemoveTodo(event){
+    const removeLi = event.target.parentElement;
+    removeLi.remove();
 }
 
 function printToDo(inputTdV){
     const listElement = document.createElement("li");
     const SpanElement = document.createElement("span");
     const ButtonElement = document.createElement("button");
-    ButtonElement.innerText = "🙅‍♂️";
+    ButtonElement.innerText = "X";
     ButtonElement.addEventListener("click",RemoveTodo);
     SpanElement.innerText=inputTdV;
     listElement.appendChild(SpanElement);
