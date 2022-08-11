eb8c44f0cf5c83bc8af560d7982ca7f36e4134e6
MONOTYPEEE <spair0039@gmail.com>
2022-08-12 00:44:58 +0900

    이제 배경이 배경처럼 작동합니다

--- JavaScrpt/Wallpaper.js
+++ JavaScrpt/Wallpaper.js
@@ -1,8 +1,6 @@
-const walp = ["1.jpg","2.jpg","3.jpg","6762.jpg","5894.jpg"];
+const wallpaperArray = ["1.jpg","2.jpg","3.jpg","6762.jpg","5894.jpg"];
 
-const chosenImage = walp[Math.floor(Math.random() * walp.length)];
+const chosenImage = wallpaperArray[Math.floor(Math.random() * wallpaperArray.length)];
 
-const bgImg = document.createElement("img");
-
-bgImg.src="image/" + chosenImage;
-document.body.appendChild(bgImg);
+document.body.style.backgroundImage = `url(image/${chosenImage})`
+console.log("image/" + chosenImage);
